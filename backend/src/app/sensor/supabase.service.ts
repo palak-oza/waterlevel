8d5e2a66cbb18aa97bb179fcab059ce4ff040cfc
Palak Oza <86180372+palak-oza@users.noreply.github.com>
2024-04-18 01:22:09 +0530

    Update supabase.service.ts

--- backend/src/app/sensor/supabase.service.ts
+++ backend/src/app/sensor/supabase.service.ts
@@ -8,8 +8,8 @@ export class SupabaseService {
 
   constructor() {
     this.supabase = createClient(
-      'https://oyxwpkznmawsavvqhgxl.supabase.co',
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im95eHdwa3pubWF3c2F2dnFoZ3hsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDg3NzE0ODUsImV4cCI6MjAyNDM0NzQ4NX0.SD1ZiGx2r9u3m6lY6iC7MMKEgvxW4NT5AvyT7qlZzfw'
+     apikey:'',
+     apiurl:''
     );
   }
 
